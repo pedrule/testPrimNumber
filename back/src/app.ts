0dde99f350adae413c1cbfd2d3c81c32c78c5d29
Alibert <pierre.cozum@gmail.com>
2018-05-21 20:26:05 +0200

    add console.log

--- back/src/app.ts
+++ back/src/app.ts
@@ -5,3 +5,6 @@ import { CalcPremiumNumber } from "./calc0fPremium";
 
 export const number = new CalcPremiumNumber();
 number.target = 10001;
+number.getResult().then((result) => {
+    console.log(`result of the ${number.target}nth prime number is ${result}`)
+})
